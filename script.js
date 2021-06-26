d1a432165d06880c2b0bd2c762b3606320f8bdea
Raviraj Dulange <redhood.dumb@gmail.com>
2021-06-26 08:49:09 +0530

    Add current script(initial commit)

---
+++ script.js
@@ -0,0 +1,36 @@
+// Navbar
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle("change");
+});
+// End of Navbar
+
+// Section 2 Video
+const video = document.querySelector(".video");
+const btn = document.querySelector(".buttons i");
+const bar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = "far fa-pause-circle";
+    video.style.opacity = "0.7";
+  } else {
+    video.pause();
+    btn.className = "far fa-play-circle";
+    video.style.opacity = "0.3";
+  }
+};
+
+btn.addEventListener("click", () => {
+  playPause();
+});
+
+video.addEventListener("timeupdate", () => {
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%`;
+});
+// End of Section 2 Video

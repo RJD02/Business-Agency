a73ed01b3443eeb09cfdc9fd9ebe6ca030cf8c37
Raviraj Dulange <redhood.dumb@gmail.com>
2021-06-27 13:01:24 +0530

    Add necessary changes

--- script.js
+++ script.js
@@ -4,57 +4,53 @@ const navbar = document.querySelector(".navbar");
 
 menu.addEventListener("click", () => {
   navbar.classList.toggle("change");
-  menu.classList.toggle("change");
+  menu.classList.toggle('change')
 });
 // End of Navbar
 
 // Section 2 Video
-const video = document.querySelector(".video");
-const btn = document.querySelector(".buttons i");
-const bar = document.querySelector(".video-bar");
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons button i');
+const bar = document.querySelector('.video-bar')
 
 const playPause = () => {
-  if (video.paused) {
-    video.play();
-    btn.className = "far fa-pause-circle";
-    video.style.opacity = "0.7";
+  if(video.paused) {
+    video.play()
+    btn.className = 'far fa-pause-circle'
+    video.style.opacity = '.7'
   } else {
-    video.pause();
-    btn.className = "far fa-play-circle";
-    video.style.opacity = "0.3";
+    video.pause()
+    btn.className = 'far fa-play-circle'
+    video.style.opacity = '.3'
   }
-};
+}
 
-btn.addEventListener("click", () => {
-  playPause();
-});
+btn.addEventListener('click', () => {
+  playPause()
+})
 
-video.addEventListener("timeupdate", () => {
+
+video.addEventListener('timeupdate', () => {
+  console.log(video.currentTime, video.duration)
   const barWidth = video.currentTime / video.duration;
-  bar.style.width = `${barWidth * 100}%`;
-  if (video.ended) {
-    btn.className = "far fa-play-circle";
-    video.style.opacity = ".3";
-  }
-});
+  bar.style.width = `${barWidth * 100}%` 
+  if(video.ended) {
+    btn.className = 'far fa-play-circle'
+    video.style.opacity = '.3'
+  } 
+})
 // End of Section 2 Video
 
-// Section 3 Pricing Cards
-var swiper = new Swiper(".mySwiper", {
-  observer: true,
-  observeParents: true,
-  effect: "coverflow",
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: "auto",
+  slidesPerView: 'auto',
   coverflowEffect: {
     rotate: 70,
     stretch: 0,
     depth: 100,
     modifier: 1,
     slideShadows: true,
-  },
+  }
 });
-
-// swiper.update();
-// End of Section 3 Pricing Cards
